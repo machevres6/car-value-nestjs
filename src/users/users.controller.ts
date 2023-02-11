b552fbe4056d9c1b92a5fc2af5642191d4d77341
machevres6 <machevres6@gmail.com>
2023-02-11 15:31:57 -0400

    Udemy Lecture 88

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,26 @@
-import { Body, Controller, Delete, Get, Patch, Post, Param, Query, NotFoundException, Session } from '@nestjs/common';
+import { 
+	Body,
+	Controller,
+	Delete, 
+	Get, 
+	Patch, 
+	Post, 
+	Param, 
+	Query, 
+	NotFoundException, 
+	Session, 
+	UseGuards 
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-use.dto';
 import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
+
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -14,13 +30,19 @@ export class UsersController {
 		private authService: AuthService
 	) {}
 
-	@Get('/whoami')
-	async whoAmI(@Session() session: any) {
-		const whoAmIUser = await this.usersService.findOne(session.userId);
+	// @Get('/whoami')
+	// async whoAmI(@Session() session: any) {
+	// 	const whoAmIUser = await this.usersService.findOne(session.userId);
 
-		if (!whoAmIUser) {
-			throw new NotFoundException('Nobody is Logged In');
-		}
+	// 	if (!whoAmIUser) {
+	// 		throw new NotFoundException('Nobody is Logged In');
+	// 	}
+	// }
+
+	@Get('/whoami')
+	@UseGuards(AuthGuard)
+	whoAmI(@CurrentUser() user: User) {
+		return user;
 	}
 
 	@Post('/signup')
