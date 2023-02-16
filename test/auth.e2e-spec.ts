b9609e29a43c84b370b7a685c2d3e069b523c6bb
machevres6 <machevres6@gmail.com>
2023-02-16 19:10:56 -0400

    Udemy Lecture 122 - Finished Tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System (e2e)', () => {
     });
 
     it('handles a singup request', () => {
-        const email = 'unique_email2@email.com';
+    const email = 'unique_email2@email.com';
 
         return request(app.getHttpServer())
             .post('/auth/signup')
@@ -30,5 +30,23 @@ describe('Authentication System (e2e)', () => {
                 expect(id).toBeDefined();
                 expect(email).toEqual(email);
             })
+    });
+
+    it('signup as a new user then get the currently logged in user', async () => {
+        const email = 'asdf@asdf.com';
+
+        const res = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({ email, password: 'asdf' })
+            .expect(201)
+
+        const cookie = res.get('Set-Cookie');
+
+        const { body } = await request(app.getHttpServer())
+            .get('/auth/whoami')
+            .set('Cookie', cookie)
+            .expect(200);
+
+        expect(body.email).toEqual(email);
     });
 });
